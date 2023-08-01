fadf297509db657de0e0f43ca722c9f57af8ae53
Dorian Stoll <dorian.stoll@tmsp.io>
2023-08-01 11:42:28 +0200

    Add missing header guards to eds1.h and eds2.h
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- src/eds1.h
+++ src/eds1.h
@@ -5,6 +5,9 @@
  * Linux driver for Intel Precise Touch & Stylus
  */
 
+#ifndef IPTS_EDS1_H
+#define IPTS_EDS1_H
+
 #include <linux/hid.h>
 #include <linux/types.h>
 
@@ -33,3 +36,5 @@ int ipts_eds1_get_descriptor(struct ipts_context *ipts, u8 **desc_buffer, size_t
  */
 int ipts_eds1_raw_request(struct ipts_context *ipts, u8 *buffer, size_t size, u8 report_id,
 			  enum hid_report_type report_type, enum hid_class_request request_type);
+
+#endif /* IPTS_EDS1_H */
